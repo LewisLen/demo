7995f543e3c8291257370451b9f6b6a6b081467a
LewisLen <csshack2016@126.com>
2022-06-22 22:59:02 +0800

    feat: 调整目录

--- js-basic/model/创建型设计模式/factory-method.js
+++ js-basic/model/创建型设计模式/factory-method.js
@@ -1,35 +1,25 @@
 /**
- * 
+ *
  * 设计模式：工厂方法模式，factory-method
  * 在简单工厂模式种，如果要新增一个类，需要需改类和工厂方法两个地方，针对这个痛点，可以使用工厂方法模式。
- * 
+ *
  * 工厂方法模式本意是说将实际创建对象工作推迟到子类当中，这样核心类就变成抽象类。可以将工厂方法看作是一个实例化对象的工厂类，将创建对象的基类放在工厂方法类的原型中即可。
  */
 
-var java = function(content){
-  this.content = content;
-  (function(content){
-    var div = document.createElement('div');
-    div.innerHTML = content;
-    div.style.color = 'green';
-    document.getElementById("container").appendChild(div)
-  })(content)
-}
-
 // 篮球基类
-let BasketBall = function(ball){
+let BasketBall = function (ball) {
   this.ball = ball;
-  this.info = 'NBA';
+  this.info = "NBA";
   this.Famous = ["Jordan", "Kobe", "James"];
-}
+};
 BasketBall.prototype = {
-  getInfo: function(){
+  getInfo: function () {
     console.log("NBA");
   },
-  getFamous: function(){
+  getFamous: function () {
     console.log(this.Famous);
-  }
-}
+  },
+};
 // 足球基类
 let Football = function (ball) {
   this.ball = ball;
@@ -39,27 +29,27 @@ Football.prototype = {
   getInfo: function () {
     console.log("wordCup");
   },
-  getBallSize: function(){
-    console.log('足球大小');
-  }
+  getBallSize: function () {
+    console.log("足球大小");
+  },
 };
 
 // 安全模式
 // 无论是否使用new关键字都可以创建所需的实例，称为安全模式创建工厂类-也就是是工厂方法模式
-function createFactory(type,param){
-  if(this instanceof createFactory){
-    return new this[type](param)
-  }else{
-    return new createFactory(type,param);
+// 相对于简单工厂模式，这里新增类就行，不用改工厂function
+function createFactory(type, param) {
+  if (this instanceof createFactory) {
+    return new this[type](param);
+  } else {
+    return new createFactory(type, param);
   }
 }
 // 真正创建类的方法放到工厂函数中的原型上
 createFactory.prototype = {
   nba: BasketBall,
-  worldCup: Football
+  worldCup: Football,
 };
 // 调用方式
-let ball = createFactory('nba','篮球');
+let ball = createFactory("nba", "篮球");
 // 或
-let ball2 = new createFactory('nba','篮球');
-
+let ball2 = new createFactory("nba", "篮球");
