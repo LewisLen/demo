9fcfec360001be5aa31dd37cd12e3247b03d6de2
LewisLen <csshack2016@126.com>
2022-06-23 09:55:27 +0800

    feat: 调整现有备注

--- static/stream.js
+++ static/stream.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
-const zlib = require('zlib')
+// 压缩模块
+const zlib = require('zlib');
 
 const gzip = zlib.createGzip()
 const readstream = fs.createReadStream('./test.txt');
 const writestream = fs.createWriteStream('./test2.zip')
 
+// 通过pipe、stream的形式复制传输
 readstream.pipe(gzip).pipe(writestream)
